1cb6508375f95ad5b3573e371ef8d5044aad0437
Alexanm04 <alejandroanm04@gmail.com>
2024-09-03 12:41:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('La página ha sido cargada completamente.');
+    // Aquí puedes añadir más código JavaScript para interactividad
+});
